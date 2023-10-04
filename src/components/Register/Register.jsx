27822e9359333cf95928639b7f1f58e788bbf4c0
Zahid Hasan <zahidhasan140338@gmail.com>
2023-10-04 10:58:47 +0600

    password show toggle icon added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,54 +1,82 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../Firebase/Firebase.config";
 import { useState } from "react";
-
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Register = () => {
-    const [registerError, setRegisterError] = useState('');
-    const [success, setSuccess] = useState('');
+  const [registerError, setRegisterError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-    const handleRegister = e => {
-        e.preventDefault();
+  const handleRegister = (e) => {
+    e.preventDefault();
 
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(email, password);
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
 
-        setRegisterError('');
-        setSuccess('');
+    setRegisterError("");
+    setSuccess("");
 
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            console.log(result);
-            setSuccess('User created successfully')
-        })
-        .catch(error => {
-            console.log(error);
-            setRegisterError(error.message)
-        })
+    if (password.length < 6) {
+      setRegisterError("Password must be 6 characters long.");
+      return;
+    } else if (!/[A-Z]/.test(password)) {
+      setRegisterError(
+        "Your password should have at least one upper case character."
+      );
+      return;
     }
-    return (
-        <div>
-            {
-                registerError && <p className="text-red-700">{registerError}</p>
-            }
-            {
-                success && <p className="text-green-700">{success}</p>
-            }
-            <div className="mx-auto md:w-1/2">
-                <h2 className="text-3xl mb-4">Please Register</h2>
-                <form onSubmit={handleRegister}>
-                    <input className="mb-4 w-3/4 py-2 px-4 " type="email" name="email" placeholder="Email Address" id="" />
-                    <br />
-                    <input  className="mb-4 w-3/4 py-2 px-4" type="password" name="password" placeholder="Password" id="" />
-                    <br />
-                    <input className="btn btn-secondary mb-4 w-3/4" type="submit" value="Register" />
-                </form>
 
-            </div>
-           
-        </div>
-    );
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+        setSuccess("User created successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+        setRegisterError(error.message);
+      });
+  };
+  return (
+    <div>
+      {registerError && <p className="text-red-700">{registerError}</p>}
+      {success && <p className="text-green-700">{success}</p>}
+      <div className="mx-auto md:w-1/2">
+        <h2 className="text-3xl mb-4">Please Register</h2>
+        <form onSubmit={handleRegister}>
+          <input
+            className="mb-4 w-3/4 py-2 px-4 "
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            id=""
+            required
+          />
+          <br />
+          <input
+            className="mb-4 w-3/4 py-2 px-4"
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="Password"
+            id=""
+            required
+          />
+          <span onClick={ () => setShowPassword(!showPassword)}>
+            {
+                showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+            }
+          </span>
+          <br />
+          <input
+            className="btn btn-secondary mb-4 w-3/4"
+            type="submit"
+            value="Register"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
